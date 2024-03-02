1d7a4fe5827a30841b64c95544431591b6aa8795
KY-Lin22 <linkangyu1994@gmail.com>
2024-03-02 16:11:02 +0900

    Update create_FuncObj.m

--- src/@NLP_Penalty_Formulation/create_FuncObj.m
+++ src/@NLP_Penalty_Formulation/create_FuncObj.m
@@ -9,24 +9,23 @@ FuncObj = struct();
 FuncObj.J = Function('J', {nlp.z, nlp.p}, {nlp.J}, {'z', 'p'}, {'J'});
 FuncObj.h = Function('h', {nlp.z, nlp.p}, {nlp.h}, {'z', 'p'}, {'h'});
 FuncObj.c = Function('c', {nlp.z, nlp.p}, {nlp.c}, {'z', 'p'}, {'c'});
-% cost jacobian
-J_grad = jacobian(nlp.J, nlp.z);
-FuncObj.J_grad = Function('J_grad', {nlp.z, nlp.p}, {J_grad}, {'z', 'p'}, {'J_grad'});
+FuncObj.J_ocp = Function('J_ocp', {nlp.z, nlp.p}, {nlp.J_ocp}, {'z', 'p'}, {'J_ocp'});
+FuncObj.J_penalty = Function('J_penalty', {nlp.z, nlp.p}, {nlp.J_penalty}, {'z', 'p'}, {'J_penalty'});
+
+%% terms used in self written solver
+% cost ocp jacobian
+J_ocp_grad = jacobian(nlp.J_ocp, nlp.z);
+FuncObj.J_ocp_grad = Function('J_ocp_grad', {nlp.z, nlp.p}, {J_ocp_grad}, {'z', 'p'}, {'J_ocp_grad'});
 % constraint jacobian
 h_grad = jacobian(nlp.h, nlp.z);
 h_grad_func = Function('h_grad_func', {nlp.z}, {h_grad}, {'z'}, {'h_grad'});
 FuncObj.h_grad = sparse(h_grad_func(zeros(nlp.Dim.z, 1))); % constant matrix
 c_grad = jacobian(nlp.c, nlp.z);
 FuncObj.c_grad = Function('c_grad', {nlp.z, nlp.p}, {c_grad}, {'z', 'p'}, {'c_grad'});
-% hessian
+% cost ocp hessian
 [J_ocp_hessian, ~] = hessian(nlp.J_ocp, nlp.z);
 J_ocp_hessian_func = Function('J_ocp_hessian_func', {nlp.z}, {J_ocp_hessian}, {'z'}, {'J_ocp_hessian'});
 FuncObj.J_ocp_hessian = sparse(J_ocp_hessian_func(zeros(nlp.Dim.z, 1))); % constant matrix
-
-%% other terms
-% cost term
-FuncObj.J_ocp = Function('J_ocp', {nlp.z, nlp.p}, {nlp.J_ocp}, {'z', 'p'}, {'J_ocp'});
-FuncObj.J_penalty = Function('J_penalty', {nlp.z, nlp.p}, {nlp.J_penalty}, {'z', 'p'}, {'J_penalty'});
 % D gap function, gradient and hessian
 if isfield(nlp, 'D_gap_func')
     FuncObj.D_gap_func = Function('D_gap_func', {nlp.z}, {nlp.D_gap_func}, {'z'}, {'D_gap_func'});
